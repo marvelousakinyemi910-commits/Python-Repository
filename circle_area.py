e73d595ebcbe89ab95f337f332c2de05dfe2a605
marvelousakinyemi910-commits <marvelousakinyemi910@gmail.com>
2025-12-03 21:41:40 +0100

    circle area

---
+++ circle_area.py
@@ -0,0 +1,14 @@
+# circle area,diameter and circumference
+
+
+radius = 2
+pi = 3.14159
+
+diameter = 2*radius
+circumference = 2*pi*radius
+area = pi*(radius**2)
+
+print("Diameter: ", diameter)
+print("Circumference: ", circumference)
+print ("Area: ", area)
+
