a359ac4f3f28f525386e56590168a908633498e2
marvelousakinyemi910-commits <marvelousakinyemi910@gmail.com>
2025-12-03 21:01:30 +0100

    Delete Favorite_number.py

--- Favorite_number.py
+++
@@ -1,7 +0,0 @@
-favorite_number = 23
-guess = int(input("Guess the number: "))
-
-if guess == favorite_number:
-    print("That's my favovorite number!")
-else: 
-    print("Nice try , guess the number again!")
