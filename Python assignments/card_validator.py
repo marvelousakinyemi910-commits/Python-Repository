0bd8cd9b331dbefa621461c0006fa4df474ad5db
marvelousakinyemi910-commits <marvelousakinyemi910@gmail.com>
2025-12-23 18:54:23 +0100

    Credit card validator

--- Python assignments/card_validator.py
+++ Python assignments/card_validator.py
@@ -1,44 +1,60 @@
+def card(card_number):
 
-def card_type(card_number):
-    if card_number.startswith("4"):
-       return "Visa Card"
-    elif card_number.startswith("5"):
-        return "MasterCard"
-    elif card_number.startswith("37"):
-        return "American Express Card"
-    elif card_number.startswith("6"):
-        return "Discover Card"
+    if (card_number.isdigit()):
+        return card_number
     else:
-        return "Invalid Card"
+        raise TypeError("Card digits has non-numeric value ")
 
-
-def card_number(card_number):
-    return card_number
 
 def card_digit_length(card_number):
-    length = len(card_number)
+    length = len(card(card_number))
 
     
     if length >= 13 and length <= 16:
         return length
-    else:
-        return"Invalid card"    
+       
 
 
 
 def card_validity_status(card_number):
-    card_number = card_number
     sum_even = 0
     sum_odd = 0
-    reversed_card = card_number[::-1]
-
-    for index, digit in enumerate(reversed_card)
-        int 
+    
+    
+    
+    for index in range(1,card_digit_length(card_number)+1):
+        value = card(card_number)[card_digit_length(card_number)-index]  
+        value = int(value) 
 
         
-        if index % 2 == 0:
-            value = value* 2
+        if (index % 2 == 0):
+            value = value*2
+            if(value>9):
+                sum_even+= (value-9)
+            else:
+                sum_even+= value
+        else:
+            sum_odd += value
+    total = sum_even+ sum_odd
+    if(total%10 == 0):
+        return"Valid"
+    else:
+        return"Invalid"
 
+
+
+def card_type(card_number):
+    if (card(card_number).startswith("4")):
+        return(" Visa Card")
+    elif(card(card_number).startswith("5")):
+        return("MasterCard")
+    elif (card(card_number).startswith("37")):
+        return("American Express Card")
+    elif(card(card_number).startswith("6")):
+        return("Discover Card")
+    else:
+        raise TypeError("Card Type is not valid")
+   
 
 
 
@@ -69,6 +85,7 @@ def card_validity_status(card_number):
 
 card_number = input('Hello, Kindly Enter Card details to verify: ')
 print("Credit Card Type: ",card_type(card_number))
-print("Credit Card Number: ",card_number)
+print("Credit Card Number: ",card(card_number))
 print("Credit Card Digit Length: ",card_digit_length(card_number))
+print("Credit Card Validity status: ",card_validity_status(card_number))
 
